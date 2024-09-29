fbe2972e71caa79fd918b34d4e6b8d9eb7da62b1
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-09-28 17:44:00 -0700

    added asset obj files, and collision for couc

--- scripts/characters/player/Player.cs
+++ scripts/characters/player/Player.cs
@@ -5,37 +5,39 @@ using Incorgnito.scripts.General;
 
 public partial class Player : CharacterBody3D
 {
-    [ExportGroup("Required  Nodes")]
-    [Export]public AnimationPlayer AnimationPlayerNode { private set; get; }
-    [Export]private Sprite3D Sprite3DNode {set; get; }
-    [Export]public StateMachine StateMachineNode { private set; get; }
+	[ExportGroup("Required  Nodes")]
+	[Export]public AnimationPlayer AnimationPlayerNode { private set; get; }
+	[Export]private Sprite3D Sprite3DNode {set; get; }
+	[Export]public StateMachine StateMachineNode { private set; get; }
+	[Export]private CollisionShape3D CollisionShape3DNode { set; get; }
 
-    public Vector2 Direction;
-    
-    public override void _PhysicsProcess(double delta)
-    {
+	public Vector2 Direction;
+	
+	public override void _PhysicsProcess(double delta)
+	{
   
-    }
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        Direction = Input.GetVector(
-            GameConstants.INPUT_MV_LEFT,
-            GameConstants.INPUT_MV_RIGHT,
-            GameConstants.INPUT_MV_FWRD,
-            GameConstants.INPUT_MV_BCKWRD);
-    }
+	public override void _Input(InputEvent @event)
+	{
+		Direction = Input.GetVector(
+			GameConstants.INPUT_MV_LEFT,
+			GameConstants.INPUT_MV_RIGHT,
+			GameConstants.INPUT_MV_FWRD,
+			GameConstants.INPUT_MV_BCKWRD);
+	}
 
-    public void Flip_Sprite()
-    {
-        var isNotMovingHorizontally = (Velocity.X == 0);
+	public void Flip_Sprite()
+	{
+		var isNotMovingHorizontally = (Velocity.X == 0);
 
-        if (isNotMovingHorizontally)
-        {
-            return;
-        }
-        
-        var isMovingLeft = (Velocity.X < 0);
-        Sprite3DNode.FlipH = isMovingLeft;
-    }
+		if (isNotMovingHorizontally)
+		{
+			return;
+		}
+		
+		
+		var isMovingLeft = (Velocity.X < 0);
+		Sprite3DNode.FlipH = isMovingLeft;
+	}
 }
